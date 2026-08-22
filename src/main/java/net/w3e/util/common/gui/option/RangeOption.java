419791e4b6952f650101e28e038e41f5cf21b59a
Userhunt <dimburov@mail.ru>
2026-08-22 22:16:59 +0300

    Fix NullPointer

--- src/main/java/net/w3e/util/common/gui/option/RangeOption.java
+++ src/main/java/net/w3e/util/common/gui/option/RangeOption.java
@@ -7,15 +7,15 @@ import java.util.function.BiFunction;
 public record RangeOption<VALUE, E>(BiFunction<Component, VALUE, Component> textGetter, E value) {
 
 	public static <VALUE, E> RangeOption<VALUE, E> ofRange(E value) {
-		return new RangeOption<>((title, v) -> title.copy().append(": " + v.toString()), value);
+		return new RangeOption<>((title, v) -> title.copy().append(": " + v), value);
 	}
 
 	public static <VALUE, E> RangeOption<VALUE, E> ofCycle(E value) {
-		return new RangeOption<>((_, v) -> Component.literal(v.toString()), value);
+		return new RangeOption<>((_, v) -> Component.literal(String.valueOf(v)), value);
 	}
 
 	public static <VALUE, E> RangeOption<VALUE, E> ofFlags(E value) {
-		return new RangeOption<>((_, v) -> Component.literal(v.toString()), value);
+		return new RangeOption<>((_, v) -> Component.literal(String.valueOf(v)), value);
 	}
 
 }
